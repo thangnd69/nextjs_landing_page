dd31ac66143903ab0e55e179290609366aa90ed3
DESKTOP-FJA9VLT\Dell <huylm6@fpt.com>
2025-01-07 18:45:24 +0700

    fisrt commit

---
+++ components/Features/featuresData.tsx
@@ -0,0 +1,91 @@
+import { Feature } from "@/types/feature";
+
+const featuresData: Feature[] = [
+  {
+    id: 1,
+    icon: "/images/icon/image-metatag.png",
+    title: "Service Operation ITSM",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
+  },
+  {
+    id: 2,
+    icon: "/images/icon/icon-02.svg",
+    title: "Application Runtimes",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
+  },
+  {
+    id: 3,
+    icon: "/images/icon/icon-03.svg",
+    title: "DevOps Automation",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
+  },
+  {
+    id: 4,
+    icon: "/images/icon/icon-04.svg",
+    title: "Networking",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
+  },
+  {
+    id: 5,
+    icon: "/images/icon/icon-05.svg",
+    title: "Cloud Security",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
+  },
+  {
+    id: 6,
+    icon: "/images/icon/icon-06.svg",
+    title: "Unified Portal",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
+  },
+  {
+    id: 7,
+    icon: "/images/icon/icon-06.svg",
+    title: "Backup & Restore",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
+  },
+  {
+    id: 8,
+    icon: "/images/icon/icon-06.svg",
+    title: "Storage Cloud Storage",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
+  },
+  {
+    id: 9,
+    icon: "/images/icon/icon-06.svg",
+    title: "App Template",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
+  },
+  {
+    id: 10,
+    icon: "/images/icon/icon-06.svg",
+    title: "IAM",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
+  },
+  {
+    id: 11,
+    icon: "/images/icon/icon-06.svg",
+    title: "Logging, Monitoring, Alerting",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
+  },
+  {
+    id: 12,
+    icon: "/images/icon/icon-06.svg",
+    title: "Container registry  ",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
+  },
+  
+];
+
+export default featuresData;
